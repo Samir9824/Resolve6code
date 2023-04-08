943fdcf2de00fdca783f8a4c5c6da6cdb35928e3
Samir9824 <samir.soni@hotmail.com>
2023-04-08 11:00:50 -0400

    Pushing my Second Program

---
+++ GitPackage/src/Git_Another_Class.java
@@ -0,0 +1,11 @@
+
+public class Git_Another_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ram is great Teacher and my second project");
+
+	}
+
+}
