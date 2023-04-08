e1e4e20c29e05deee7e7487a733418e41ca580b9
Samir9824 <samir.soni@hotmail.com>
2023-04-08 10:44:54 -0400

    Test

---
+++ GitPackage/src/MySampleGitClass.java
@@ -0,0 +1,10 @@
+
+public class MySampleGitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My Git Project Example Sharing");
+	}
+
+}
